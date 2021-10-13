cae9e3c772f4514ef7ae78e4c107dc80beef8140
realTronsi <63553774+realTronsi@users.noreply.github.com>
2021-10-13 18:49:34 +0000

    primitive file read

--- include/readfile.c
+++ include/readfile.c
@@ -1,5 +1,24 @@
 #include "./readfile.h"
 
-int* freadf(char* path){
+#define true 1
+#define false 0
+
+int freadf(char* path, int* buffer, size_t* size){
+	FILE* file = fopen(path, "rb");
+
+	if(file == NULL){
+		return false;
+	}
+
+	// file size
+	fseek(file, 0, SEEK_END);
+	size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	int* data = calloc(size, int);
+	data = file;
 	
+	buffer = data;
+
+	return true;
 }
--- src/sat/pasm16/pasm16.c
+++ src/sat/pasm16/pasm16.c
@@ -1,3 +1,5 @@
+#include "../../../include/readfile.h"
+
 int main(int argc, char* argv){
 	return 0;
 }
