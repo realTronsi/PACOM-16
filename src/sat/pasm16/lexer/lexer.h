783c35ee8e4810559ac7d429464309a36abc8a2b
realTronsi <63553774+realTronsi@users.noreply.github.com>
2021-10-14 04:45:45 +0000

    v0.0.3 - lexer barebone init

---
+++ src/sat/pasm16/lexer/lexer.h
@@ -0,0 +1,14 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+enum {
+	SYS,
+	LOAD,
+	SET,
+	ADDL,
+	SUBL,
+	MUTL,
+	DIVL
+} Tokens;
+
+#endif
--- src/sat/pasm16/pasm16.c
+++ src/sat/pasm16/pasm16.c
@@ -6,7 +6,9 @@ int main(int argc, char* argv){
 
 	if(argc > 0){
 		if(freadf(argv[0], code, length) == true){
-			
+			// setup stack
+			short stack[32768];
+
 		}
 	}
 	return 0;
